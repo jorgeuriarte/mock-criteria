8176b4a0d3b9717711627d8bc27929b9ac888014
Jorge Uriarte <jorge.uriarte@omelas.net>
2011-11-13 10:12:31 +0100

    Small refactorings

--- src/groovy/mockcriteria/CriteriaMocker.groovy
+++ src/groovy/mockcriteria/CriteriaMocker.groovy
@@ -4,66 +4,56 @@ class CriteriaMocker {
 	def element
 	boolean fits = true
 	def CriteriaMocker(element) {
-		println "ELemento: ${element}"
 		this.element = element
 	}
 
 	def eq(a, b) {
-		if (fits) {
-			fits = (element."${a}" == b)
-		}
+		test { element."${a}" == b }
 	}
 
 	def eqProperty(a, b) {
-		if (fits) {
-			fits = (element."${a}" == element."${b}")
-		}
+		test { element."${a}" == element."${b}" }
 	}
 
 	def gt(a, b) {
-		if (fits) {
-			fits = (element."${a}" > b)
-		}
+		test { element."${a}" > b }
 	}
 
 	def gtProperty(a, b) {
-		if (fits) {
-			fits = (element."${a}" > element."${b}")
-		}
+		test { element."${a}" > element."${b}" }
 	}
 
 	def ge(a, b) {
-		if (fits) {
-			fits = (element."${a}" >= b)
-		}
+		test { element."${a}" >= b }
 	}
 
 	def geProperty(a, b) {
-		if (fits) {
-			fits = (element."${a}" >= element."${b}")
-		}
+		test { element."${a}" >= element."${b}" }
 	}
 	def lt(a, b) {
-		if (fits) {
-			fits = (element."${a}" < b)
-		}
+		test { element."${a}" < b }
 	}
 
 	def ltProperty(a, b) {
-		if (fits) {
-			fits = (element."${a}" <= element."${b}")
-		}
+		test { element."${a}" <= element."${b}" }
 	}
 
 	def le(a, b) {
-		if (fits) {
-			fits = (element."${a}" <= b)
-		}
+		test { element."${a}" <= b }
 	}
 
 	def leProperty(a, b) {
+		test {element."${a}" <= element."${b}"}
+	}
+
+	def like(a, b) {
+		def pattern = /^${b.replaceAll('%','.+')}$/
+		test { element."${a}" ==~ pattern }
+	}
+
+	Closure test = { comp ->
 		if (fits) {
-			fits = (element."${a}" <= element."${b}")
+			fits = comp()
 		}
 	}
 
--- test/unit/mockcriteria/DomainTests.groovy
+++ test/unit/mockcriteria/DomainTests.groovy
@@ -20,49 +20,73 @@ class DomainTests extends GrailsUnitTestCase {
     }
 
     void testEmptyFilterReturnsFullList() {
-		assert domains.size() == Domain.createCriteria().list() {
+		assert domains.size() == results {
 			
 		}.size()
     }
 
     void testEqFilterOnBoolean() {
-    	assert 2 == Domain.createCriteria().list() {
+    	assert 2 == results {
     		eq('active', true)
     	}.size()
     }
 
     void testEqFilterOnNumber() {
-    	assert "joseba" == Domain.createCriteria().list() {
+    	assert "joseba" == results {
     		eq('id', 3)
     	}[0].name
     }
 
     void testEqFilterOnString() {
-    	assert 3 == Domain.createCriteria().list() {
+    	assert 3 == results {
     		eq('name', 'joseba')
     	}[0].id
     }
 
     void testMultipleEqOnSring() {
-    	assert 2 == Domain.createCriteria().list() {
+    	assert 2 == results {
     		eq('address', 'Bilbao')
     		eq('active', true)
     	}.size()
     }
 
     void testGtGeLtLeOnNumber() {
-    	assert (domains.size() - 1) == Domain.createCriteria().list() {
+    	assert (domains.size() - 1) == results {
     		gt('id', 1)
     	}.size()
-    	assert (domains.size()x) == Domain.createCriteria().list() {
+    	assert (domains.size()) == results {
     		ge('id', 1)
     	}.size()
-    	assert 1 == Domain.createCriteria().list() {
+    	assert 1 == results {
     		lt('id', 2)
     	}.size()
-    	assert 2 == Domain.createCriteria().list() {
+    	assert 2 == results {
     		le('id', 2)
     	}.size()
     }
 
+    void testLikeOnString() {
+    	assert 3 == results {
+    		like('address', 'Bil%')
+    	}.size()
+    	assert 4 == results {
+    		like('address', '%il%')
+    	}.size()
+    	assert 1 == results {
+    		like('address', '%iribil%')
+    	}.size()
+    	assert 1 == results {
+    		like('address', '%lla')
+    	}.size()
+    	assert 0 == results {
+    		like('address', 'Bil')
+    	}.size()
+    	assert 3 == results {
+    		like('address', 'B%ao')
+    	}.size()
+    }
+
+    private def results(Closure clos) {
+    	Domain.createCriteria().list(clos)
+    }
 }
